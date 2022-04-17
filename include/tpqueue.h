78a91530e9df90b108567404958607af666566bf
ErrorDa <99288583+ErrorDa@users.noreply.github.com>
2022-04-18 00:26:49 +0300

    Update tpqueue.h

--- include/tpqueue.h
+++ include/tpqueue.h
@@ -4,7 +4,33 @@
 
 template<typename T, int size>
 class TPQueue {
-  // реализация шаблона очереди с приоритетом на кольцевом буфере
+private:
+  T arr[size];
+  int start;
+  int stop;
+public:
+  TPQueue() :start(0), stop(0) { }
+  void push(T x) {
+    if ((stop - start) >= size)
+      throw std::string("fail");
+    else {
+      int i = stop++;
+      while ((i > start) && (arr[i % size].prior < x.prior)) {
+        arr[(i + 1) % size] = arr[i % size];
+        i--;
+      }
+      arr[(i + 1) % size] = x;
+      }
+  }
+  T pop() {
+    return arr[(start++) % size];
+  }
+  void show() {
+    for (int i = 0;i < size;i++) {
+      std::cout << arr[i].ch << ' ';
+    }
+    std::cout << "\n";
+  }
 };
 
 struct SYM {
