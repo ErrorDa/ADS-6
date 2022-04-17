97ec19a66dd43366b4ba40974ebf1e804ec24d1a
ErrorDa <99288583+ErrorDa@users.noreply.github.com>
2022-04-18 00:34:35 +0300

    Update tpqueue.h

--- include/tpqueue.h
+++ include/tpqueue.h
@@ -4,35 +4,28 @@
 
 template<typename T, int size>
 class TPQueue {
-private:
-  T arr[size];
+ private:
+  T arr[100];
   int start;
   int stop;
-public:
-  TPQueue() :start(0), stop(0) { }
+ public:
+TPQueue() :start(0), stop(0) { }
   void push(T x) {
-    if ((stop - start) >= size)
-      throw std::string("fail");
-    else {
+    if ((stop - start) >= size) {
+      throw "fail";
+    } else {
       int i = stop++;
       while ((i > start) && (arr[i % size].prior < x.prior)) {
         arr[(i + 1) % size] = arr[i % size];
         i--;
       }
       arr[(i + 1) % size] = x;
-      }
+    }
   }
   T pop() {
     return arr[(start++) % size];
   }
-  void show() {
-    for (int i = 0;i < size;i++) {
-      std::cout << arr[i].ch << ' ';
-    }
-    std::cout << "\n";
-  }
 };
-
 struct SYM {
   char ch;
   int prior;
